5b6be307c55cce3b1c586c2c900b415db57e950d
rental-PC yoshida <l2508939949as@gmail.com>
2025-10-16 13:06:05 +0900

    年齢変更のプログラムを追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,17 +1,36 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
+  /*
+  private String name = "Enami Kouji";
+  private String age = "37";
+
+   @GetMapping("/studentInfo")
+  public String getStudentInfo(){
+    return name + " " + age + "歳";
+  }
+
+  @PostMapping("/studentInfo")
+  public  void setStudentInfo(String name,String age){
+  this.name = name;
+    this.age = age;
+  }
+  @PostMapping("/studentName")
+  public void updeteStudentName(String name){
+    this.name = name;
+  }
   @GetMapping("/hello")
     public String hello(){
       return "Hello, World!";
@@ -23,5 +42,35 @@ public class StudentManagementApplication {
   }
 
 
+   */
+
+  private String name;
+  private String age;
+  private Map<String,String> students = new HashMap<>(Map.of("山田　太郎","26","山田　花子","23"));
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
+  @GetMapping("/studentInformation")
+  public  String getStudensInformation(){
+    StringBuffer result = new StringBuffer();
+    for (Map.Entry<String,String>entry : students.entrySet()){
+      name = entry.getKey();
+      age = entry.getValue();
+      result.append(name).append("：").append(age).append("歳\n");
+    }
+    return result.toString();
+  }
+  @PostMapping("/studentInformation")
+  public String  updateStudentAge(@RequestParam String name, @RequestParam String age) {
+    if (students.containsKey(name)) {
+      students.put(name, age);  // 年齢を更新
+      return name + "さんの年齢を" + age + "歳に変更しました。";
+    } else {
+      return name + "さんは登録されていません。";
+    }
+  }
+
 
 }
+
