ab1ad5356bd26dd4f423b0721bfa22c23323de82
rental-PC yoshida <l2508939949as@gmail.com>
2025-10-21 14:58:27 +0900

    第7回演習課題　全件表示を追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,36 +1,66 @@
 package raisetech.StudentManagement;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+  @Autowired
+  private  StudentRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
   /*
-  private String name = "Enami Kouji";
-  private String age = "37";
-
-   @GetMapping("/studentInfo")
-  public String getStudentInfo(){
-    return name + " " + age + "歳";
+   @GetMapping("/student")
+  public String getStudent(@RequestParam String name){
+    Student student = repository.searchByName(name);
+    return student.getName() + " " + student.getAge() + "歳";
+  }
+   */
+  @GetMapping("/student")
+  public String getAllStudent(){
+    List<Student> studentList = repository.StudentList();
+    StringBuffer result = new StringBuffer();
+    for (Student student : studentList){
+      result.append(student.getName())
+          .append(" ")
+          .append(student.getAge())
+          .append("歳\n");
+    }
+    return result.toString();
   }
 
-  @PostMapping("/studentInfo")
-  public  void setStudentInfo(String name,String age){
-  this.name = name;
-    this.age = age;
+
+  @PostMapping("/student")
+  public  void registerStudent(String name,int age){
+   repository.registarStudent(name,age);
+
   }
-  @PostMapping("/studentName")
-  public void updeteStudentName(String name){
-    this.name = name;
+  @PatchMapping("/student")
+  public void updeteStudent(String name,int age){
+    repository.updateStudent(name,age);
   }
+  @DeleteMapping("/student")
+  public void deleteStudent(String name){
+    repository.deleteStudent(name);
+  }
+
+
+  /*
   @GetMapping("/hello")
     public String hello(){
       return "Hello, World!";
@@ -41,6 +71,14 @@ public class StudentManagementApplication {
     return "こんにちは、Webのプログラム開発の世界にようこそ！";
   }
    */
+
+
+
+
+  /*
+  @Autowired
+  private  StudentRepository repository;
+
 
   private String name;
   private String age;
@@ -68,6 +106,9 @@ public class StudentManagementApplication {
       return name + "さんは登録されていません。";
     }
   }
+
+   */
+
 
 }
 
