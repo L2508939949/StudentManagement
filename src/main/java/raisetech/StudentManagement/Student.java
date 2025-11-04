8528781f8525322940a30ff4cccb759bdc165e01
rental-PC yoshida <l2508939949as@gmail.com>
2025-11-04 13:25:12 +0900

    第10回演習課題　 キャメルケース、インデントの修正、不要なコメントを削除

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @Setter
 public class Student {
 
-  private  String studentid;
+  private  String StudentID;
   private  String name;
   private  String kanaName;
   private  String nickname;
