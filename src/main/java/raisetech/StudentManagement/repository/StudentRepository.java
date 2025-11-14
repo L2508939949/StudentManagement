b1e93b00022c77d3c80fd0e509e909757a8ad34d
rental-PC yoshida <l2508939949as@gmail.com>
2025-11-14 14:04:05 +0900

    演習課題第16回

--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -3,6 +3,7 @@ package raisetech.StudentManagement.repository;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentsCourses;
