1c13c61a9f21a40d19c787c4eb224aa6e6ff70ea
rental-PC yoshida <l2508939949as@gmail.com>
2025-12-03 13:36:16 +0900

    演習課題第20回1回目のコミット

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
@@ -20,7 +22,7 @@ import raisetech.StudentManagement.data.StudentsCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
 
   private StudentService service;
@@ -33,12 +35,10 @@ public class StudentController {
       }
 
   @GetMapping("/studentList")
-  public String getStudentList(Model model){
+  public List<StudentDetail> getStudentList(){
     List<Student> students = service.searchStudentList();
     List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-
-    model.addAttribute("studentList",converter.convertStudentDetails(students,studentsCourses));
-    return "studentList";
+    return converter.convertStudentDetails(students,studentsCourses);
   }
 
   @GetMapping("/updateStudentForm")
@@ -67,7 +67,7 @@ public class StudentController {
   }
 
   @PostMapping("/updateStudent")
-  public String updateStudent(@ModelAttribute StudentDetail studentDetail,
+  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail,
       @RequestParam("oldCourseID") String oldCourseID,
       @RequestParam("courseStartday") String courseStartdayStr,
       @RequestParam("courseEndday") String courseEnddayStr) {
@@ -81,7 +81,7 @@ public class StudentController {
     }
     service.updateStudent(studentDetail.getStudent());
     service.updateCourses(studentDetail.getStudent().getStudentID(), oldCourseID, studentDetail.getCourse());
-    return "redirect:/studentList";
+    return ResponseEntity.ok("更新処理が成功しました。");
   }
 
 
