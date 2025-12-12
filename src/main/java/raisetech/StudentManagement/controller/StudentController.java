cfaaaa0866b7e4ac927d0ac2ec052c812ec12293
rental-PC yoshida <l2508939949as@gmail.com>
2025-12-12 11:31:20 +0900

    演習課題第25回1回目のコミット

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,5 +1,6 @@
 package raisetech.StudentManagement.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -46,6 +47,7 @@ public class StudentController {
    *
    * @return 受講生詳細一覧(全件)
    */
+  @Operation(summary = "一覧検索", description = "受講生の一覧を検索します。")
   @GetMapping("/studentList")
   public List<StudentDetail> getStudentList(){
     return service.searchStudentList();
@@ -84,6 +86,7 @@ public class StudentController {
    * @return 実行結果
    */
 
+
   @PutMapping("/updateStudent")
   public ResponseEntity<String> updateStudent(
       @RequestBody @Valid StudentDetail studentDetail,
@@ -117,6 +120,8 @@ public class StudentController {
    * @param studentDetail 　受講生情報と受講生コース情報
    * @return 実行結果
    */
+
+  @Operation(summary ="受講生登録", description = "受講生を登録します。")
   @PostMapping("/registerStudent")
   public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail) {
     List<StudentCourse> courses= studentDetail.getStudentCourseList();
